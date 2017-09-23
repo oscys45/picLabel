336f047a45988f7c9a41442887353394cd731fc9
JuncoJ <james_junco@partech.com>
2017-09-23 01:21:23 -0400

    Added fields to user data model. Changed client greeting to show display name instead of user name.

--- picLabel/src/app/models/user.ts
+++ picLabel/src/app/models/user.ts
@@ -3,4 +3,8 @@ import { baseModel } from './base-model'
 export class User extends baseModel{
   username: string;
   password: string;
+  firstName: string;
+  lastName: string;
+  displayName: string;
+  email: string;
 }
--- picLabelAPI/models/account.js
+++ picLabelAPI/models/account.js
@@ -5,7 +5,11 @@ let passportLocalMongoose = require('passport-local-mongoose');
 let Account = new Schema({
     _id: { type: Schema.ObjectId, auto: true },
     username: String,
-    password: String
+    password: String,
+    firstName: String,
+    lastName: String,
+    displayName: String,
+    email: String
 });
 
 Account.plugin(passportLocalMongoose);
